7f629a55d06e9d64a05e23b81d8db4aefdd129b6
kanika8108 <106243599+kanika8108@users.noreply.github.com>
2022-05-25 16:43:48 +0530

    Add files via upload

---
+++ trianglepatteren.py
@@ -0,0 +1,96 @@
+# Write a code, that counts the numbers between 20 and 40 and then 
+# print its count.
+
+# n=[50,34,23,13,27,21,45,36]
+# i=0
+# c=0
+# while i<len(n):
+#     if n[i]>20 and n[i]<40:
+#        c+=1
+#     i+=1
+# print(c)
+
+# Write a code that prints the maximum in this list.
+# n = [50, 40, 23, 70, 56, 12, 5, 10, 7]
+# i=0
+# m=0
+# while i<len(n):
+#     if n[i]>m:
+#         m=n[i]
+#     i+=1
+# print(m)
+
+
+#  Write a code that the reverses the order of the items means in opposite order.
+# places=["delhi", "gujrat", "rajasthan", "punjab", "kerala"]
+# i=-1
+# while i<len(places):
+#     print(places[i])
+#     i+=1
+
+# Write a code that prints the second maximum number in this list.
+# n = [50, 40, 23, 70, 56, 12, 5, 10, 7]
+# i=0
+# j=0
+# m=0
+# while i<len(n):
+#   if n[i]>m:
+#     m=n[i]
+#   i+=1
+# m2=0
+# while j<len(n):
+#   if n[j]>m2 and n[j]!=m:
+#     m2=n[j]
+#   j+=1
+# print(m2)
+
+
+# print odd number from the list.
+# n=[23,45,32,25,46,33,71,90]
+# i=0
+# while i<len(n):
+#     if n[i]%2==1:
+#         print(n[i])
+#     i+=1
+
+# check if list is palindrome or not 
+# k=['n','i','t','i','n']
+# m=k
+# i=-1
+# j=[]
+# while i<len(k):
+#     j+=k[i]
+#     i-=1
+#     if j==m:
+#         print(k,"is palindrome")
+#     else:
+        # (k,"is not palindrome")
+
+a=input("write it   ")
+# ab=input("enter any name")
+b=a.strip()
+print(b)
+
+
+
+
+
+# binary to decimal
+# b=[1,0,0,1,1,0,1,1]
+# i=-1
+# j=0
+# l=0
+# while j<len(b):
+#     k=(b[i]*2**j)
+#     l+=k
+#     i+=-1
+#     j+=1
+# print(l)
+
+
+
+
+
+
+
+
